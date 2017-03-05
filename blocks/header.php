afa9f5bd6873390049fa5dfaf56ad56c7a8397c7
Nikita <yemelianovn@tut.by>
2017-03-05 18:44:49 +0300

    add newuser page

--- blocks/header.php
+++ blocks/header.php
@@ -4,6 +4,6 @@
         <a href="http://emelya_learning/new%20www/index.php" title="Перейти на главную">Green news</a>
     </div>
     <div id="regAuth">
-        <a href="reg.php">Log In</a> | <a href="auth.php">Sign In</a>
+        <a href="reg.php">Log In</a> | <a href="signin.php">Sign In</a>
     </div>
 </header>
